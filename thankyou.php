c60224e45df901540ec3aff5332841ad8796f7ee
supportdcbindia <120620782+supportdcbindia@users.noreply.github.com>
2025-09-17 18:47:45 +0530

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -1,37 +1,29 @@
-<?php include("header-1.php");?>
-<section id="page-banner" >
-    
-        <div class="row">
-            <div class="col-md-12">
-                <div class="breadcrumbs-area">
-                    <img src="images/Breadcums/Thank-You.jpg" class="img-responsive">
+<?php include('header.php'); ?>
+<!-- Blog area start -->
+<section class="blog__area-6 blog__animation productpage-details-container">
+    <div class="container g-0 pb-110">
+        <div class="row pb-90">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
+                <div class="sec-title-wrapper">
+                    <h2 class="sec-title-2 animation__char_come">THANK YOU</h2>
                 </div>
             </div>
         </div>
-</section>
-<div class="breadcrumb clearfix">
-  <div class="container">
-    <div class="row">
-      <div class="main_title">
-        <em class="title">THANK YOU</em>
-      </div>
-      <div class="bread-right">
-        <a class="home" href="index.php" title="Return to Home"><i class="fa fa-home"></i>Home</a>
-        <i>|</i>
-        <h2><a href="javascript:;">THANK YOU</a></h2>
-      </div>
+
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
+                <div class="product-items-box">
+                    <div class="product-items-img">
+                        <h2 class="text-center" style="color: green;">Thankyou!!Successfully Submitted</h2>
+            <h4 class="text-center thnak4">Your Inquiry has been successfully submitted to the concern department.You
+                will get a reply within 24 hours from our side.....!!</h4>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
     </div>
-  </div>
-</div>
-
-<section class="contact-section pdt-100 pdb-65">
-	<div class="container">
-		<div class="row">
-			<h2 class="text-center" style="color: green;">Thankyou!!Successfully Submitted</h2>
-                        <h4 class="text-center thnak4">Your Inquiry has been successfully submitted to the concern department.You will get a reply within 24 hours from our side.....!!</h4>
-		</div>
-	</div>
 </section>
-
-<div class="clearfix"></div>
-<?php include("footer.php");?>
+<!-- Blog area end -->
+<?php include('footer.php'); ?>
