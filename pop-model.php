a6b572944cf9c8db24ac68f5fb3ca86329b88c87
supportdcbindia <120620782+supportdcbindia@users.noreply.github.com>
2025-09-17 16:12:30 +0530

    Update pop-model.php

--- pop-model.php
+++ pop-model.php
@@ -11,7 +11,7 @@
       </li>
       <li class="download-pdf none-li inquiery-icon  imgnone">
         <a data-href="pdf/CI PUMPSET Performance Booklet.pdf" href="javascript:;" class="click2">
-          <span class="icon"> <i class="fa fa-file-pdf-o"></i></span> <span class="btn-text">Catalogue</span>
+          <span class="icon"> <i class="fa fa-file-pdf"></i></span> <span class="btn-text">Catalogue</span>
         </a>
       </li>
      <li class="whataspp-icon none-li imgnone">
@@ -235,7 +235,7 @@
       <a class="nav_up click1" href="javascript:;" style="color:#FFF; font-size:12px;font-weight:600;"><i class="fa fa-envelope" style="margin-right: 5px;"></i> Enquire Now</a>
     </div>
     <div class="book-app" style="background: #222;">
-      <a class="nav_up click2" data-href="pdf/CI PUMPSET Performance Booklet.pdf" href="javascript:;" style="color:#FFF; font-size:12px;font-weight:600;"><i class="fa fa-file-pdf-o" style="margin-right: 5px;"></i> Catalogue</a>
+      <a class="nav_up click2" data-href="pdf/CI PUMPSET Performance Booklet.pdf" href="javascript:;" style="color:#FFF; font-size:12px;font-weight:600;"><i class="fa fa-file-pdf" style="margin-right: 5px;"></i> Catalogue</a>
     </div>
    <div class="book-app" style="background: #2db640;">
     <a onclick="gtag('event', 'send', { 'event_category': 'click on whatsapp', 'event_action': 'Mobile', 'event_label': '+916354916823' });" href="https://api.whatsapp.com/send?phone=916354916823&amp;text=Hello team Waterman Industries Pvt. Ltd., I was going through your website and wish to get connected for product discussion" style="color:#FFF; font-size:12px;font-weight:600;"><i class="fab fa-whatsapp" style="margin-right: 5px;"></i> Whatsapp</a>
